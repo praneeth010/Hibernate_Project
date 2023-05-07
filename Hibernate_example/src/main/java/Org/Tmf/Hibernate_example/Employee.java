57115444f3d6cf8b7ed967845d415fc4039932ee
praneeth010 <129770007+praneeth010@users.noreply.github.com>
2023-05-07 22:15:07 +0530

    Add files via upload

---
+++ Hibernate_example/src/main/java/Org/Tmf/Hibernate_example/Employee.java
@@ -0,0 +1,44 @@
+package Org.Tmf.Hibernate_example;
+
+public class Employee {
+	private int id;  
+	private String firstName,lastName,gender,email;
+	private long PhoneNumber;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getPhoneNumber() {
+		return PhoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		PhoneNumber = phoneNumber;
+	}
+
+}
